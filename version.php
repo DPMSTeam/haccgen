cf23df9e63fd35cf10b68583adf2bba0a321ae16
Shagun Gupta <shagung.dynamicpixel@gmail.com>
2026-02-17 11:06:06 +0530

    Bump plugin release version to 1.0.1

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_haccgen';
 $plugin->version = 2026021600;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = ['editor_tiny' => ANY_VERSION];
